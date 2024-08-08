0d3436d57e9b8cb5fcaf3ab6ee876d97f156e79c
Adam Bersanukaev <adambers2020@gmail.com>
2024-08-08 14:38:33 +0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+// server.js
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Настройки для получения __dirname в ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Укажите путь к папке со статическими файлами после сборки
+const publicPath = path.resolve(__dirname, ".");
+app.use(express.static(publicPath));
+
+// Обслуживание всех маршрутов основным HTML-файлом
+app.get("*", (_, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+// Запуск сервера на указанном порту
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
